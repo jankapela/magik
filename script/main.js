164cbf1bb0faa11f62d384a0e993741a5ed355b9
Adam Szerszeń <adam_szerszen@interia.pl>
2018-12-18 17:43:28 +0100

    Changed main.js: removed vanilla.js, added jquery event attach on click;

--- script/main.js
+++ script/main.js
@@ -1,43 +1,50 @@
+var mainPageModel = {
+    navigationCollapsed: true
+};
+
 //smooth scroll
-$(document).ready(function() {
+$(document).ready(function () {
+    var nav = $('.nav');
 
-	$('a[href^="#"]').on('click', function(event) {
+    $('a[href^="#"]').on('click', function (event) {
 
-		var target = $( $(this).attr('href') );
+        var target = $($(this).attr('href'));
 
-		if( target.length ) {
-			event.preventDefault();
-			$('html, body').animate({
-				scrollTop: target.offset().top
-			}, 800);
-		}
-	});
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 800);
+        }
+    });
 
+    /* NAV */
+    var button = $('.nav-button');
+
+    var sprawdz = 0;
+    var links = $('.nav-link');
+
+    links.each(function (index, link) {
+        link.onclick = slideNav.bind(nav);
+    });
+
+    button.click(slideNav);
+
+    function slideNav() {
+        if (mainPageModel.navigationCollapsed) {
+            $('.nav').addClass('show');
+            $('.nav').removeClass('hide');
+            toggleNavigation();
+        } else {
+            $('.nav').addClass('hide');
+            $('.nav').removeClass('show');
+            toggleNavigation();
+        }
+    }
 });
 
-/* NAV */
 
-var button = document.querySelector('.nav-button');
-var nav = document.querySelector('.nav');
-var sprawdz = 0;
-var links = [link1, link2, link3, link4, link5, link6];
 
-button.addEventListener('click', slideNav);
-
-function slideNav(){
-
-    if(sprawdz == 0) {
-        nav.style.width = "100%";
-        sprawdz = 1;
-	}
-
-    else {
-        nav.style.width = "0";
-        sprawdz = 0;
-	}
-
-}
-
-for (var i = 0; i <= 6; i++) {
-    links[i].addEventListener("click", slideNav);
+function toggleNavigation() {
+    mainPageModel.navigationCollapsed = !mainPageModel.navigationCollapsed;
 }
